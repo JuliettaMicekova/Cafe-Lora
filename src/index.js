0ba47d1e1e83f0722b096d59a7416c7c17ab844c
Julia Micekova <julka_micekova@yahoo.co.uk>
2021-05-01 12:46:51 +0200

    set-up of order button

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ document.querySelector('#nav-btn').addEventListener('click', () => {
   document.querySelector('nav').classList.toggle('nav-closed');
 });
 
-const menuItemElm = document.querySelectorAll('.nav-link');
+const menuItemElm = document.querySelectorAll('nav-link');
 
 for (let i = 0; i < menuItemElm.length; i += 1) {
   menuItemElm[i].addEventListener('click', () => {
@@ -25,3 +25,19 @@ for (let i = 0; i < menuItemElm.length; i += 1) {
 // Na tlačíko se třídou order-btn pověste posluchač, při kliknutí změní nápis na tlačítku na Zrušit. Zároveň přidá na prvek se třídou drink__cup třídu drink__cup--selected.
 // Při opětovném kliknutí na tlačítko chceme zařídit, aby se objednávka zrušila a změny na prvku .drink__cup i na tlačítku .order-btn se vrátily (odebrala se třída drink__cup--selected a vrátil se text tlačítka na Objednat). Tohoto můžete docílit například tak, že si vytvoříte globální proměnnou ordered, která bude obsahovat true nebo false podle toho, zde je nápoj objednaný či nikoliv.
 // Ve chvíli, kdy máte objednávání funkční commitněte váš kód se smysluplnou zprávnou a pushněte jej.
+
+const orderBtnElm = document.querySelector('.order-btn');
+const drinCupElm = document.querySelector('.drink-cup');
+
+let ordered = false;
+orderBtnElm.addEventListener('click', () => {
+  if (ordered === false) {
+    orderBtnElm.textContent = 'Zrušit';
+    drinCupElm.classList.add('drink_cup--selected');
+    ordered = true;
+  } else if (ordered === true) {
+    orderBtnElm.textContent = 'Objednat';
+    orderBtnElm.classList.remove('drink_cup--selected');
+    ordered = false;
+  }
+});
