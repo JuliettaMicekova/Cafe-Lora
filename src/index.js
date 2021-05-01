e8983d56ada6a8b68c8d639f6af577bd2a2b7aec
Julia Micekova <julka_micekova@yahoo.co.uk>
2021-05-01 12:20:05 +0200

    navigation bar set

--- src/index.js
+++ src/index.js
@@ -2,9 +2,20 @@ import './style.css';
 
 console.log('funguju!');
 
-const navBtnElm = document.querySelector('.nav-closed');
-const navElm = document.querySelector('nav');
+// Chceme zařídit, aby se na úzkých displejích po kliknutí na hamburger ikonku zobrazovala navigace. Zatím se to neděje a tím přichází ke slovu JavaScript.
 
-navBtnElm.addEventListener('click', () => {
-  navBtnElm.classList.toggle('nav-closed');
+// V JavaScriptu vyberte ze stránky ikonku #nav-btn. Připojte k ní posluchač události click. Tento posluchač prvku nav odebere nebo přidá třídu nav-closed. Klikáním na ikonku tak můžeme zobrazovat nebo skrývat navigaci.
+// Navigaci budeme chtít schovat i po přesunutí na nějakou sekci. Připojte tedy posluchač události také na všechny položky navigace. Zařiďte, aby se navigace při kliknutí na libovolnou její položku schovala. Zde se vám jistě bude hodit metoda querySelectorAll.
+// Jakmile je váš kód funkční, proveďte commmit s hezky popisnou zprávou a pushněte do vzdáleného repozitáře.
+
+document.querySelector('#nav-btn').addEventListener('click', () => {
+  document.querySelector('nav').classList.toggle('nav-closed');
 });
+
+const menuElm = document.querySelectorAll('.nav-link');
+
+for (let i = 0; i < menuItemElm.length; i += 1) {
+  menuItemElm[i].addEventListener('click', () => {
+    document.querySelector('nav').classList.toggle('nav-closed');
+  });
+}
